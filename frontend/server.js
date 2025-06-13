4169ddcb55ef7ac114fd0a7d5692a299af947fe9
GustavoMSantoss <moreira.santos.gus@gmail.com>
2025-06-12 21:29:49 -0300

    Atualizando o front 8

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8129;
 
 // Serve arquivos estáticos do build do React
 app.use(express.static(path.join(__dirname, 'build')));
